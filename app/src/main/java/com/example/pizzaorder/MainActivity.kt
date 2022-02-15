1f276d8d504224cdff928091deb9f5ce9bc94edc
ShemarS <42276240+ShemarS@users.noreply.github.com>
2022-02-14 19:08:38 -0500

    UI creation
    UI stuff

--- app/src/main/java/com/example/pizzaorder/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorder/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.pizzaorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pizzaList = listOf("BBQ Chicken", "Hawaiian", "Margherita", "Pepperoni")
+        val pizzaAdapter = ArrayAdapter<String>(
+            this, android.R.layout. simple_spinner_dropdown_item, pizzaList)
+        pizzaSelectionSpinner.adapter = pizzaAdapter
     }
+
+    fun radioClick(view: View){
+// check which radio button was clicked
+        if (view == mediumRadioButton){
+            Toast.makeText(this, "Medium was clicked", Toast.LENGTH_SHORT).show()
+        }
+        else if (view == largeRadioButton){
+
+        }
+        else if (view == exLargeradioButton){
+
+        }
+        else {
+
+        }
+    }
+
+
+
 }
